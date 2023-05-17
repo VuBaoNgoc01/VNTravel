4e89318f73a0c6a81d61c260315a7c3adb76c45e
VuBaoNgoc01 <vbngoc211201@gmail.com>
2023-05-17 08:21:07 +0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Parallax Scrolling Effect
 
+
 let bg = document.getElementById("bg");
 let sea = document.getElementById("sea");
 let moutain1 = document.getElementById("moutain1");
@@ -8,22 +9,22 @@ let moutain3 = document.getElementById("moutain3");
 let moutain4 = document.getElementById("moutain4");
 let moutain5 = document.getElementById("moutain5");
 let boat = document.getElementById("boat");
-let cloud1 = document.getElementById("cloud1");
 let cloud2 = document.getElementById("cloud2");
 let title = document.getElementById("title");
 
 window.addEventListener('scroll', function () {
     var value = window.scrollY;
 
-    bg.style.top = value * 0.5 + 'px';
-    moutain1.style.top = value * 0.5 + 'px';
-    moutain2.style.top = value * 0.5 + 'px';
-    moutain3.style.top = value * 0.5 + 'px';
-    moutain4.style.left = value * -0.5 + 'px';
+    bg.style.top = value * 1 + 'px';
+    moutain1.style.top = value * 1 + 'px';
+    moutain2.style.top = value * 1 + 'px';
+    moutain3.style.top = value * 1 + 'px';
+    moutain4.style.left = value * -1 + 'px';
     moutain5.style.left = value * 0.5 + 'px';
-    boat.style.top = value * 0.5 + 'px';
-    boat.style.left = value * 2.5 + 'px';
-    title.style.marginTop = value * 1.5 + 'px';
+    boat.style.top = value * 1 + 'px';
+    boat.style.left = value * 2 + 'px';
+    title.style.marginTop = value * 2.5 + 'px';
+
 });
 
 $(document).ready(function () {
@@ -44,7 +45,7 @@ $(document).ready(function () {
     $("#scroll-to-top").click(function () {
         $('html, body').animate({
             scrollTop: 0
-        }, 500)
+        }, 100)
     })
 });
 
@@ -83,4 +84,128 @@ $('.slider-home').slick({
         }
     ]
 });
+
+
+$('.slide-tour').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 800,
+    dots: false,
+    autoplay: false,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-light fa-chevron-left'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-light fa-chevron-right'></i></button>",
+    responsive: [
+        {
+            breakpoint: 1230,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('.list-cmt').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    dots: true,
+    autoplay: false,
+    arrows: false,
+    //prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-light fa-chevron-left'></i></button>",
+    //nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-light fa-chevron-right'></i></button>",
+});
+
+$('.slide-blog').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 800,
+    dots: false,
+    autoplay: false,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-light fa-chevron-left'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-light fa-chevron-right'></i></button>",
+    responsive: [
+        {
+            breakpoint: 1230,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('.slide-partner').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    speed: 800,
+    dots: false,
+    autoplay: false,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-light fa-chevron-left'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-light fa-chevron-right'></i></button>",
+    responsive: [
+        {
+            breakpoint: 1230,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+
+
+// $('.slider-gallery').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     rows: 3,
+//     slidesToScroll: 3,
+//     speed: 800,
+//     dots: false,
+//     autoplay: false,
+//     arrows: true,
+//     prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-light fa-chevron-left'></i></button>",
+//     nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-light fa-chevron-right'></i></button>",
+// });
 
